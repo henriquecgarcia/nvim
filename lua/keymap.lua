310d5c2af270a38b39e121a7dfbb3a3a237ff555
Henrique Garcia <henriquecampanhagarcia@gmail.com>
2025-04-28 18:47:37 -0300

    feat: a lot of stuff

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -8,3 +8,29 @@ map('v', '<C-s>', '<Esc>:w<CR>', opts)
 
 -- Fechar janela com Ctrl + Q
 map('n', '<C-q>', ':q<CR>', opts)
+
+-- Função para mover linhas ou blocos de código para cima
+function MoveLineUp()
+	vim.cmd("m .-2")
+	vim.cmd("normal! ==")
+end
+
+-- Função para mover linhas ou blocos de código para baixo
+function MoveLineDown()
+	vim.cmd("m .+1")
+	vim.cmd("normal! ==")
+end
+
+-- Função para duplicar a linha atual ou bloco de código selecionado
+function DuplicateLineOrSelection()
+	vim.cmd("t .")
+end
+
+-- Mapeamentos de teclas para as funções acima
+vim.api.nvim_set_keymap('n', '<A-k>', ':lua MoveLineUp()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-j>', ':lua MoveLineDown()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-d>', ':lua DuplicateLineOrSelection()<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-d>', 'y`>p`<P', { noremap = true, silent = true })
